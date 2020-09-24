1ce8b058aed1df11d01f5d4dc6d5c7ca80e7404d
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-09-24 09:34:25 +0300

    Adde MainActivity

--- app/src/main/java/com/example/prostoshoop/MainActivity.java
+++ app/src/main/java/com/example/prostoshoop/MainActivity.java
@@ -1,14 +1,108 @@
 package com.example.prostoshoop;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    int quantity = 0;
+    Spinner spinner;
+    ArrayList spinnerArrayList;
+    ArrayAdapter spinnerAdapter;
+
+    HashMap goodsMap;
+    String goodsName;
+    double price;
+    EditText userNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userNameEditText = findViewById(R.id.nameEditText);
+
+//        createSpinner();
+//        createMap();
     }
+
+    void createSpinner() {
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        spinnerArrayList = new ArrayList();
+
+        spinnerArrayList.add("tshirt");
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter);
+    }
+
+    void createMap() {
+        goodsMap = new HashMap();
+        goodsMap.put("tshort", 30.0);
+        goodsMap.put("jacket", 150.0);
+        goodsMap.put("sweater", 50.0);
+    }
+
+    public void increaseQuantity(View view) {
+        quantity = quantity + 1;
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+    }
+
+    public void decreaseQuantity(View view) {
+        quantity = quantity - 1;
+        if (quantity < 0) {
+            quantity = 0;
+        }
+        TextView quantityTextView = findViewById(R.id.quantityTextView);
+        quantityTextView.setText("" + quantity);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        goodsName = spinner.getSelectedItem().toString();
+        price = (double)goodsMap.get(goodsName);
+        TextView priceTextView = findViewById(R.id.priceTextView);
+        priceTextView.setText("" + quantity * price);
+
+        ImageView goodsImageView = findViewById(R.id.goodsImageView);
+
+        switch (goodsName) {
+            case "tshort":
+                goodsImageView.setImageResource(R.drawable.tshort);
+                break;
+            case "jacket":
+                goodsImageView.setImageResource(R.drawable.jacket);
+                break;
+            case "sweater":
+                goodsImageView.setImageResource(R.drawable.sweter);
+                break;
+            default:
+                goodsImageView.setImageResource(R.drawable.tshort);
+                break;
+        }
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
