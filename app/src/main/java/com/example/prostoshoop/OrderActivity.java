8541ed4a4ee336525efda9e875c6894d32dcc081
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-09-28 10:01:42 +0300

    AddToCart activity

--- app/src/main/java/com/example/prostoshoop/OrderActivity.java
+++ app/src/main/java/com/example/prostoshoop/OrderActivity.java
@@ -1,4 +1,48 @@
 package com.example.prostoshoop;
 
-public class OrderActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class OrderActivity extends AppCompatActivity {
+
+    String[] addresses = {"mashman007@gmail.com"};
+    String subject = "Prostoshop Order";
+    String emailText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Intent receivedOrderIntent = getIntent();
+        String userName = receivedOrderIntent.getStringExtra("userNameForIntent");
+        String goodsName = receivedOrderIntent.getStringExtra("goodsName");
+        int quantity = receivedOrderIntent.getIntExtra("quantity", 0);
+        double price = receivedOrderIntent.getDoubleExtra("price", 0);
+        double orderPrice = receivedOrderIntent.getDoubleExtra("orderPrice", 0);
+
+        emailText = "Customer name: " + userName + "\n"+
+                "Goods name: " + goodsName + "\n" +
+                "Quantity: " + quantity + "\n" +
+                "Price: " + price+ "\n" +
+                "Order Price: " + orderPrice;
+
+        TextView orderTextView = findViewById(R.id.orderTextView);
+        orderTextView.setText(emailText);
+    }
+
+    public void submitOrder(View view) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("*/*");
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
 }
